adb2ee42eed842ccc8003507c302fb90757a5c6e
Nilkanth Patel <patelnilkanthj@gmail.com>
2019-07-04 08:56:39 -0700

    Add remote images

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,6 +25,16 @@ module.exports = {
         icon: `src/images/midtype-logo.png`
       }
     },
-    `gatsby-plugin-offline`
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-plugin-remote-images`,
+      options: {
+        nodeType: 'assets',
+        imagePath: 'location',
+        name: 'midtypeImage'
+      }
+    }
   ]
 };
